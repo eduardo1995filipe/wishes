9aba3555ec771abfd9e423f41dd2a3f4011c2986
eduardo1995filipe <eduardo_95@yahoo.com>
2018-12-09 10:43:06 +0000

    improved menu, already adds new wishes and list them after adding(for now only in memory)

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
+import WishManager
+import Wish
+
+def main():
+
+        wish_manager = WishManager.WishManager()
+
+        exit = False
+        while exit is False:
+                print("")
+                print("Main Menu:")
+                print("1 - Check wishes")
+                print("2 - Add new wish")
+                print("3 - Unwish")
+                print("4 - Exit")
+                value = int(input("Choose an option:"))
+                if value == 4:
+                        exit = True
+                elif value == 1:
+                        wish_list = wish_manager.getAllWishes()
+                        for wish in wish_list:
+                                print(wish)
+                elif value == 2:
+                        description = input("insert a description:")
+                        value = float(input("insert a value:"))
+                        savings = float(input("insert your savings for this wish(pass if you don't have savings yet):"))
+                        wish_manager.createWish(description = description, value = value, savings = savings)
+                        print("Expense successfully added :D")
+
+                        
+
+
 if __name__ == "__main__":
     print("Welcome to wishes! Post your wish here and amount money to buy it! :D")
+    main()
