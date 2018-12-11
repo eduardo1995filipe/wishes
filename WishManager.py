d949266eece21281256c663ed117530077d9bbf4
eduardo1995filipe <eduardo_95@yahoo.com>
2018-12-11 14:41:37 +0000

    - added a funtion to get the next id in the wish list.

--- WishManager.py
+++ WishManager.py
@@ -7,10 +7,21 @@ class WishManager:
 
     def createWish(self, description, value, savings = None):
         if savings is None:
-            new_wish = Wish.Wish(description ,value)
+            new_wish = Wish.Wish(self.getNextID(), description ,value)
         else:
             new_wish = Wish.Wish(description ,value, savings)
         self.wish_list.append(new_wish)
 
     def getAllWishes(self):
         return self.wish_list
+
+    def removeWishById(self, id):
+        #remove list
+        pass
+
+    def getNextID(self):
+        max_id = 0  
+        for element in self.wish_list:
+            if element.id > max_id:
+                max_id = element.id
+        return (max_id + 1)
