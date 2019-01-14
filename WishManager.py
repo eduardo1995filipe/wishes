038705e362dda3c65d10907a6542215f14c61c73
eduardo1995filipe <eduardo_95@yahoo.com>
2019-01-14 15:14:13 +0000

    - now it's possible to increase wish value:
    - wishes are saved and loaded from a txt file

--- WishManager.py
+++ WishManager.py
@@ -4,6 +4,8 @@ class WishManager:
 
     def __init__(self):
         self.wish_list = []
+        self.filename = "wishes.txt"
+        self.loadWishes()
 
     def createWish(self, description, value, savings = None):
         if savings is None:
@@ -27,3 +29,32 @@ class WishManager:
             if element.id > max_id:
                 max_id = element.id
         return (max_id + 1)
+
+    def loadWishes(self):
+        loaded_wish_list = []
+        try:
+            file = open(self.filename, "r")
+        except FileNotFoundError:
+            file = open(self.filename, "w+")
+        for line in file:
+            wish_array = line.split(";")
+            new_wish = Wish.Wish(int(wish_array[0]), wish_array[1], float(wish_array[2]), float(wish_array[3]))
+            loaded_wish_list.append(new_wish)
+        self.wish_list = loaded_wish_list
+        file.close()
+        return
+        
+    
+    def commitChanges(self):
+        file = open(self.filename, "w")
+        for wish in self.wish_list:
+            file.write(str(wish.id) + ";" + wish.description + ";" +  str(wish.value) + ";" + str(wish.savings) +"\n")
+        file.close()
+        return
+
+    def changeSavingsById(self,id, new_savings):
+        for wish in self.wish_list:
+            if wish.id == id:
+                wish.savings = wish.savings + new_savings
+                return True
+        return False
--- main.py
+++ main.py
@@ -12,27 +12,46 @@ def main():
                 print("1 - Check wishes")
                 print("2 - Add new wish")
                 print("3 - Unwish")
-                print("4 - Exit")
-                value = int(input("Choose an option:"))
+                print("4 - Increase wish savings")
+                print("5 - Exit")
+
+                value = int(input("Choose an option: "))
+
                 if value == 4:
+                        wish_list = wish_manager.getAllWishes()
+                        for wish in wish_list:
+                                print(wish)
+                        
+                        id = int(input("insert the id of the wish that you want to increase the savings: "))
+                        while True:
+                                new_savings = float(input("insert here the amount you want to add to your savings for turn this wish real: "))
+                                if (wish_manager.changeSavingsById(id, new_savings)):
+                                        print("savings of this wish succesfully incremented!")
+                                        break
+                                else:
+                                        print("[Error] Please check if you inserted a number or if the amount you added exceed the wish value!")
+                if value == 5:
                         exit = True
+                        wish_manager.commitChanges()
+                        print("bye!!!\n\n")
                 elif value == 1:
                         wish_list = wish_manager.getAllWishes()
                         for wish in wish_list:
                                 print(wish)
+                        print("\n\n")
                 elif value == 2:
                         description = input("insert a description: ")
                         value = float(input("insert a value: "))
                         savings = float(input("insert your savings for this wish(pass if you don't have savings yet): "))
                         wish_manager.createWish(description = description, value = value, savings = savings)
-                        print("Expense successfully added :D")
+                        print("Expense successfully added :D\n\n")
                 elif value == 3:
                         wish_list = wish_manager.getAllWishes()
                         for wish in wish_list:
                                 print(wish)
                         id = int(input("insert the id of the wish that you want to unwish: "))
                         wish_manager.removeWishById(id)
-                        print("successfully unwished!")
+                        print("successfully unwished!\n\n")
 
                         
 
