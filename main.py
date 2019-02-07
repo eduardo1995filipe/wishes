77de24f0813e77a1fbc58c7e827eccb5a6c5ee23
eduardo1995filipe <eduardo_95@yahoo.com>
2019-02-07 14:16:41 +0000

    - Input check implemented in all inputs; - Bugs on flow control of the program fixed.

--- main.py
+++ main.py
@@ -4,33 +4,41 @@ import WishManager
 def main():
 
         wish_manager = WishManager.WishManager()
-
         exit = False
         while exit is False:
-                print("")
-                print("Main Menu:")
-                print("1 - Check wishes")
-                print("2 - Add new wish")
-                print("3 - Unwish")
-                print("4 - Increase wish savings")
-                print("5 - Exit")
-
-                value = int(input("Choose an option: "))
-
+                while True:
+                        try:
+                                print("")
+                                print("Main Menu:")
+                                print("1 - Check wishes")
+                                print("2 - Add new wish")
+                                print("3 - Unwish")
+                                print("4 - Increase wish savings")
+                                print("5 - Exit")
+                                value = int(input("Choose an option: "))
+                                break
+                        except:
+                                print("Insert a valid option!")
                 if value == 4:
                         wish_list = wish_manager.getAllWishes()
                         for wish in wish_list:
                                 print(wish)
                         
-                        id = int(input("insert the id of the wish that you want to increase the savings: "))
                         while True:
-                                new_savings = float(input("insert here the amount you want to add to your savings for turn this wish real: "))
-                                if (wish_manager.changeSavingsById(id, new_savings)):
-                                        print("savings of this wish succesfully incremented!")
+                                try:
+                                        id = int(input("insert the id of the wish that you want to increase the savings: "))
+                                        new_savings = float(input("insert here the amount you want to add to your savings for turn this wish real: "))
+                                                     
+                                        if (wish_manager.changeSavingsById(id, new_savings)):
+                                                print("savings of this wish succesfully incremented!")
+                                                break
+                                        else:
+                                                print("[Error] Please check if you inserted a number or if the amount you added exceed the wish value!")
+                                                continue
                                         break
-                                else:
-                                        print("[Error] Please check if you inserted a number or if the amount you added exceed the wish value!")
-                if value == 5:
+                                except:
+                                        print("Insert a valid option!")
+                elif value == 5:
                         exit = True
                         wish_manager.commitChanges()
                         print("bye!!!\n\n")
@@ -40,20 +48,29 @@ def main():
                                 print(wish)
                         print("\n\n")
                 elif value == 2:
-                        description = input("insert a description: ")
-                        value = float(input("insert a value: "))
-                        savings = float(input("insert your savings for this wish(pass if you don't have savings yet): "))
-                        wish_manager.createWish(description = description, value = value, savings = savings)
+                        while True:
+                                try:
+                                        description = str(input("insert a description: "))
+                                        value = float(input("insert a value: "))
+                                        break
+                                except:
+                                        print("Insert a valid value please!")
+                        wish_manager.createWish(description = description, value = value, savings = 0.0)
                         print("Expense successfully added :D\n\n")
                 elif value == 3:
                         wish_list = wish_manager.getAllWishes()
                         for wish in wish_list:
                                 print(wish)
-                        id = int(input("insert the id of the wish that you want to unwish: "))
+                        while True:
+                                try:
+                                        id = int(input("insert the id of the wish that you want to unwish: "))
+                                        break
+                                except:
+                                        print("insert a valid value!")
                         wish_manager.removeWishById(id)
                         print("successfully unwished!\n\n")
-
-                        
+                else:
+                        print("choose a valid option!")                
 
 
 if __name__ == "__main__":
